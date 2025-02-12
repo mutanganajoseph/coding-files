d15c93a1952f3ece68baf6af3188284204ae2fd4
Jmutangana <josephmutangana65@gmail.com>
2025-02-12 18:00:09 +0200

    Auto-update: 2025-02-12 18:00:09

---
+++ python_basics/permutations.py
@@ -0,0 +1,30 @@
+import itertools
+import math 
+import os
+
+
+try:
+
+    n = int(input("\n\nEnter a number to get permutations: "))
+
+
+
+    if n <= 0:
+        print(f"\nThere is No Permutation for n <= 0,  {n}.")
+
+    if n <= 6 and n >0:   
+
+        numbers = list(range(1, n + 1))
+        permutations = list(itertools.permutations(numbers))
+        print(f"\nHere is {n}! = {math.factorial(n)} Permutations.\n")
+        
+        for perm in permutations:
+            
+            print(perm)
+        
+    if n > 6:
+        print("\nThe Number You Enter is Very Large. Try, >0 , <=6")
+
+except ValueError:
+    _= os.system("clear")
+    print("\nInvalid Input.\n")
