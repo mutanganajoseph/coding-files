1630281304bc101c5565c62b8593ccc90d4b0e22
Jmutangana <josephmutangana65@gmail.com>
2025-02-16 10:00:06 +0200

    Auto-update: 2025-02-16 10:00:06

--- python_basics/permutations.py
+++ python_basics/permutations.py
@@ -1,36 +1,33 @@
-# import itertools
-# import math 
-# import os
+import itertools
+import math 
+import os
 
 
-# try:
+try:
 
-#     n = int(input("\n\nEnter a number to get permutations: "))
+    n = int(input("\n\nEnter a number to get permutations: "))
 
 
 
-#     if n <= 0:
-#         print(f"\nThere is No Permutation for n <= 0,  {n}.")
+    if n <= 0:
+        print(f"\nThere is No Permutation for n <= 0,  {n}.")
 
-#     if n <= 6 and n >0:   
+    if n <= 6 and n >0:   
 
-#         numbers = list(range(1, n + 1))
-#         permutations = list(itertools.permutations(numbers))
-#         print(f"\nHere is {n}! = {math.factorial(n)} Permutations.\n")
+        numbers = list(range(1, n + 1))
+        permutations = list(itertools.permutations(numbers))
+        print(f"\nHere is {math.factorial(n)} Permutations.\n")
         
-#         for perm in permutations:
+        for perm in permutations:
             
-#             print(perm)
+            print(perm)
         
-#     if n > 6:
-#         print("\nThe Number You Enter is Very Large. Try, >0 , <=6")
+    if n > 6:
 
-# except ValueError:
-#     _= os.system("clear")
-#     print("\nInvalid Input.\n")
+        print("\nThe Number You Enter is Very Large. Try n>0 , n<=6\n")
 
-n = input("enter A Number: ")
+except ValueError:
+    _= os.system("clear")
+    print("\nInvalid Input.\n")
 
-numbers = ()
 
-numbers.a
