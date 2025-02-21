1ad157ac84923c8b2ea879dcc5645fbe34565631
Jmutangana <josephmutangana65@gmail.com>
2025-02-21 16:00:09 +0200

    Auto-update: 2025-02-21 16:00:09

--- python_basics/array.py
+++ python_basics/array.py
@@ -1,13 +1,18 @@
-import numpy as np 
+import numpy as np
 
 A = np.array([
-    [2,3],
-    [3,2],
-    [4,-2]
+    [2, 3, 3],
+    [3, 2, -5],
+    [4, -2, 3]
 ])
 
-B = np.array([1,1,1])
+B =[1, 1]
+B.append(2)
 
-det_A = np.linalg.norm(A)
-print(A.shape)
+# Append B as a new row to A
+A = np.vstack([A, B])
 
+#append new element
+# A = np.append(A, 2)
+
+print(A)
