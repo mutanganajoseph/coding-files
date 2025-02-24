aa4f9781da1a1974ec663eb5dcb1800d0bc43609
Jmutangana <josephmutangana65@gmail.com>
2025-02-24 20:00:07 +0200

    Auto-update: 2025-02-24 20:00:07

--- python_basics/linear-algebra.py
+++ python_basics/linear-algebra.py
@@ -3,12 +3,10 @@ import numpy as np
 A = np.array([1,2,3])
 B = np.array([4,5,6])
 C = np.array([7,8,9])
-
 D = np.array([[2,3],[4,5]])
 
 
 matrix_a = np.array([[1,2,3],[4,5,6],[-7,8,9]])
-
 matrix_b = np.array([[7,8],[9,10],[11,12]])
 
 magnitude_a = np.linalg.norm(A)
@@ -27,9 +25,9 @@ surface_area = area_ab+area_ac+area_bc
 volume = abs(np.dot(A, np.cross(B,C)))
 
 cos_theta = dotProduct_ab / (magnitude_a * magnitude_b)
-
 angle_radian = np.arccos(cos_theta)
 angle_degrees = np.degrees(angle_radian)
+
 det_matrix_a= np.linalg.det(matrix_a)
 eigenvalues, eigenvectors = np.linalg.eig(D)
 
@@ -44,6 +42,12 @@ print(f"Magnitude of A = {magnitude_b:.1f}")
 print(f"Determinant of Matrix A is {det_matrix_a:.1f}")
 print(f"Dot product of A and B = {dotProduct_ab}")
 print(f"Croos product of A and B = {crossProduct_ab}\nMatrix Product = \n{matrixProduct}\n")
-print(f"EigenValues of matrix D is{eigenvalues}\nEigenVectors of Matrix D is:\n{eigenvectors}\n")
 
 
+for i in range(len(eigenvalues)):
+    print(f"Eigenvalue {i+1}: {eigenvalues[i]:.2f}")
+
+# Print the eigenvectors with formatting
+for i in range(len(eigenvectors[0])):
+    print(f"Eigenvector {i+1}: [{eigenvectors[0, i]:.2f}, {eigenvectors[1, i]:.2f}]")
+print("\n")
