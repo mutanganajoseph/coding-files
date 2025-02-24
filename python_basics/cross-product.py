b256737b7a2f74b4c4c774f56f6493a4b1b187a7
Jmutangana <josephmutangana65@gmail.com>
2025-02-24 17:00:09 +0200

    Auto-update: 2025-02-24 17:00:09

--- python_basics/cross-product.py
+++ python_basics/cross-product.py
@@ -1,12 +1,18 @@
 import numpy as np 
-cross = lambda v, u: np.linalg.norm(np.cross(u,v))
+
+A = np.array([1,2,3])
+B = np.array([-1,3,2])
+
+matrix_a = np.array([[1,2,3],[4,5,6]])
+
+matrix_b = np.array([[7,8],[9,10],[11,12]])
+
+magnitude_a = np.linalg.norm(A)
+dotProduct_ab = np.dot(A,B)
+crossProduct_ab = np.cross(A,B)
+matrixProduct = np.dot(matrix_a, matrix_b)
+
+print(f"Magnitude of A = {magnitude_a}")
+print(f"Dot product of A and B = {dotProduct_ab}\nCroos product of A and B = {crossProduct_ab}\nMatrix Product = \n{matrixProduct}")
 
 
-u = np.array([3,-2,1])
-v = np.array([1,4,3])
-
-dotProduct_uv = sum([x*y for x,y in zip(u,v) ])
-
-sum = u+v
-mult = u*v
-print(f"{dotProduct_uv}\n{sum}\n{mult}")
