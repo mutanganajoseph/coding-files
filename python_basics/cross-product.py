248dd157e153ac9ec06d31dd8035f54e905f146f
Jmutangana <josephmutangana65@gmail.com>
2025-02-24 19:00:07 +0200

    Auto-update: 2025-02-24 19:00:07

--- python_basics/cross-product.py
+++ python_basics/cross-product.py
@@ -4,15 +4,16 @@ A = np.array([1,2,3])
 B = np.array([4,5,6])
 C = np.array([7,8,9])
 
+D = np.array([[2,3],[4,5]])
 
-matrix_a = np.array([[1,2,3],[4,5,6]])
+
+matrix_a = np.array([[1,2,3],[4,5,6],[-7,8,9]])
 
 matrix_b = np.array([[7,8],[9,10],[11,12]])
 
 magnitude_a = np.linalg.norm(A)
 magnitude_b = np.linalg.norm(B)
-dotProduct_ab = np.dot(A,B)
-dot = sum([x*y for x,y in zip(A,B)])
+dotProduct_ab = A @ B
 crossProduct_ab = np.cross(A,B)
 crossProduct_ac = np.cross(A,C)
 crossProduct_bc = np.cross(B,C)
@@ -22,16 +23,27 @@ matrixProduct = np.dot(matrix_a, matrix_b)
 area_ab = np.linalg.norm(crossProduct_ab)
 area_ac = np.linalg.norm(crossProduct_ac)
 area_bc = np.linalg.norm(crossProduct_bc)
-
 surface_area = area_ab+area_ac+area_bc
-
-
 volume = abs(np.dot(A, np.cross(B,C)))
 
+cos_theta = dotProduct_ab / (magnitude_a * magnitude_b)
+
+angle_radian = np.arccos(cos_theta)
+angle_degrees = np.degrees(angle_radian)
+det_matrix_a= np.linalg.det(matrix_a)
+eigenvalues, eigenvectors = np.linalg.eig(D)
+
+
+
+
+print(f"\nAngle between A and B is {angle_degrees:.1f} degrees.")
 print(f"Area of paralleralpiped is {surface_area:.1f}")
 print(f"Volume of paralleralpiped is {volume:.1f}")
 print(f"Magnitude of A = {magnitude_a:.1f}")
 print(f"Magnitude of A = {magnitude_b:.1f}")
-print(f"Dot product of A and B = {dot}\nDot product of A and B = {dotProduct_ab}\nCroos product of A and B = {crossProduct_ab}\nMatrix Product = \n{matrixProduct}")
+print(f"Determinant of Matrix A is {det_matrix_a:.1f}")
+print(f"Dot product of A and B = {dotProduct_ab}")
+print(f"Croos product of A and B = {crossProduct_ab}\nMatrix Product = \n{matrixProduct}\n")
+print(f"EigenValues of matrix D is{eigenvalues}\nEigenVectors of Matrix D is:\n{eigenvectors}\n")
 
 
