18bbd9c89a781509cf6a8d7f8b92681c4ec5cff6
Jmutangana <josephmutangana65@gmail.com>
2025-02-27 22:00:09 +0200

    Auto-update: 2025-02-27 22:00:09

--- python_basics/jumps.py
+++ python_basics/jumps.py
@@ -2,24 +2,19 @@
 import numpy as np 
 import itertools
 
+A = (1,2,3,1).
 
 
-n = int(input("Enter a Number: "))
-numbers = list(range(1, n+1))
-perms = list(itertools.permutations(numbers))
 jumps = 0
-for perm in perms:
-
-    for i in range(len(perm)-1):
-        
-        for j in range(i+1, len(perm)):
-            if perm[i] > perm[j]:
-                jumps +=1
-
+for i in range(len(A)-1):
+    
+    for j in range(i+1, len(A)):
+        if A[i] > A[j]:
+            jumps +=1
     if jumps % 2 == 0:
         sign = "+"
+
     else:
         sign = "-"
-
-print(jumps, sign)
-
+print(A, jumps, sign)
+print()
