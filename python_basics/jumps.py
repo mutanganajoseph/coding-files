4db0e54cefaec3e54c3cc5a5d317fa92582eeac4
Jmutangana <josephmutangana65@gmail.com>
2025-02-28 07:00:01 +0200

    Auto-update: 2025-02-28 07:00:01

--- python_basics/jumps.py
+++ python_basics/jumps.py
@@ -1,19 +1,36 @@
 
-import numpy as np 
-import itertools
+# import numpy as np 
+# import itertools
 
-A = (1,2,3,1)
+# A = (1,2,3,1)
 
 
-jumps = 0
-for i in range(len(A)-1):
+# jumps = 0
+# for i in range(len(A)-1):
     
-    for j in range(i+1, len(A)):
-        if A[i] > A[j]:
-            jumps +=1
-    if jumps % 2 == 0:
-        sign = "+"
+#     for j in range(i+1, len(A)):
+#         if A[i] > A[j]:
+#             jumps +=1
+#     if jumps % 2 == 0:
+#         sign = "+"
 
-    else:
-        sign = "-"
-print(A, jumps, sign)
+#     else:
+#         sign = "-"
+# print(A, jumps, sign)
+
+try:
+    n = int(input("Enter a number: "))
+    factorial = 1
+    if n == 0:
+        print(factorial)
+    if n < 0:
+        print("No factorila for number less than zero")
+    if n > 0:
+
+        while n >=1 :
+            factorial = factorial * n
+            n-=1
+
+        print(factorial)
+except ValueError:
+    print("Invalid Number.")
