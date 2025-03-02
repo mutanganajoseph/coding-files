7ba7254888cc9a2988f54379bf1638bc40e51ab7
Jmutangana <josephmutangana65@gmail.com>
2025-03-02 13:00:08 +0200

    Auto-update: 2025-03-02 13:00:08

--- python_basics/exercise_iii.py
+++ python_basics/exercise_iii.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-area_of_paralelogram = lambda v, u: np.linalg.norm(np.cross(v, u))
+area_of_paralelogram_uv = lambda u, v: np.linalg.norm(np.cross(u, v))
+area_of_paralelogram_uw = lambda u, w: np.linalg.norm(np.cross(u, w))
+area_of_paralelogram_vw = lambda v, w: np.linalg.norm(np.cross(v,w))
+
 
 volume_of_parallelopiped = lambda u, v, w: abs(np.dot(u, np.cross(v, w)))
 
@@ -10,7 +13,7 @@ v = np.array([4, 5, 6])
 w = np.array([7, 8, 9])
 
 
-area = area_of_paralelogram(u, v)
+area = area_of_paralelogram_uv(u, v) + area_of_paralelogram_uw(u,w) + area_of_paralelogram_vw(v,w)
 volume = volume_of_parallelopiped(u, v, w)
 
 print(f"Parallelogram Area: {area:.2f}")
